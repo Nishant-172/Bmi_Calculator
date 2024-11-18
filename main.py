79fdf98de79e60667fe381303539b1723ae3bf17
Nishant-172 <nishantkumarrodezweb@gmail.com>
2024-11-18 11:53:18 +0530

    Secound Change

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #here is the code for bmi calculator
+import tkinter as tk
+from tkinter import messagebox
+
+# BMI range categories
+BMI_CATEGORIES = {
+    "Underweight": (0, 18.5),
+    "Normal weight": (18.5, 24.9),
+    "Overweight": (24.9, 29.9),
+    "Obesity": (29.9, float("inf"))
+}
